923e6851db9e6b97ad5da910d5de07a32738b961
antonio-marcos1989 <115155873+antonio-marcos1989@users.noreply.github.com>
2022-11-08 20:41:24 -0300

    .

--- webapi/app.js
+++ webapi/app.js
@@ -1,27 +1,81 @@
+//app.js
+const {MongoClient, ObjectId} = require("mongodb");
+async function connect(){
+  if(global.db) return global.db;
+  const conn = await MongoClient.connect("mongodb+srv://antonio:Antonio123@workshop.l0n83oe.mongodb.net/?retryWrites=true&w=majority");
+  if(!conn) return new Error("Can't connect");
+  global.db = await conn.db("workshop");
+  return global.db;
+}
+
 const express = require('express');
-const app = express();
-const port = 3000;
-app.use(express.urlencoded({extended:true}))
+const app = express();         
+const port = 3000; //porta padrão
+
+app.use(require('cors')());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-// Criando a rota
+//definindo as rotas
 const router = express.Router();
-router.get('/',(req,res) => res.json({message:'Funcionando!'}));
-app.use('/', router);
 
-//
-router.get('/clientes', async function(req,res,next){
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+
+/* GET clientes */
+router.get('/clientes/:id?', async function(req, res, next) {
     try{
-        const db = await connect();
+      const db = await connect();
+      if(req.params.id)
+        res.json(await db.collection("customers").findOne({_id: new ObjectId(req.params.id)}));
+      else
         res.json(await db.collection("customers").find().toArray());
     }
     catch(ex){
-        console.log(ex);
-        res.status(400).json({erro: `${ex}`});
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
     }
 })
 
-// Iniciando o servidor
+// POST /clientes
+router.post('/clientes', async function(req, res, next){
+    try{
+      const customer = req.body;
+      const db = await connect();
+      res.json(await db.collection("customers").insertOne(customer));
+    }
+    catch(ex){
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
+    }
+})
+
+// PUT /clientes/{id}
+router.put('/clientes/:id', async function(req, res, next){
+    try{
+      const customer = req.body;
+      const db = await connect();
+      res.json(await db.collection("customers").updateOne({_id: new ObjectId(req.params.id)}, {$set: customer}));
+    }
+    catch(ex){
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
+    }
+})
+
+// DELETE /clientes/{id}
+router.delete('/clientes/:id', async function(req, res, next){
+    try{
+      const db = await connect();
+      res.json(await db.collection("customers").deleteOne({_id: new ObjectId(req.params.id)}));
+    }
+    catch(ex){
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
+    }
+})
+
+app.use('/', router);
+
+//inicia o servidor
 app.listen(port);
 console.log('API funcionando!');
